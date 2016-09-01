a5835dee018d8e7e3a8d249c27a85aa268bba797
Jacob Friedmann <jacobdfriedmann@gmail.com>
2016-08-31 22:00:17 -0400

    Finish binary search

--- algorithms/binary-search/index.js
+++ algorithms/binary-search/index.js
@@ -1,20 +1,24 @@
 const defaultCompare = (a, b) => {
-    return a > b;
+  return a > b;
 };
 
-export default binarySearch(orderedArray, item, compare = defaultCompare, startIndex = 0) {
-    if (orderedArray.length === 1) {
-        return item === orderedArray[0] ? startIndex : undefined;
-    }
-    const midpoint = Math.floor(orderedArray.length / 2);
-    if (item === orderedArray[midpoint]) {
-        return midpoint + startIndex;
-    } else if (item === orderedArray[midpoint + 1]) {
-        return midpoint + startIndex + 1;
-    } else if (compare(item, orderedArray[midpoint + 1])) {
-        return binarySearch(orderedArray.slice(midpoint + 1), item, compare, startIndex + midpoint + 1);
-    } else if (!compare(item, orderedArray[midpoint])) {
-        return binarySearch(orderedArray.slice(0, midpoint), item, compare, startIndex);
-    }
-    return undefined;
+/**
+ * Recursively defined binary search implementation.
+ */
+module.exports = function binarySearch(orderedArray, item, compare = defaultCompare, minIndex = 0, maxIndex) {
+  const start = minIndex;
+  const end = maxIndex || orderedArray.length;
+  // Split the array
+  const midpoint = Math.floor((end - start)/ 2) + start;
+  if (minIndex === maxIndex) {
+    return item === orderedArray[minIndex] ? minIndex : undefined;
+  } else if (item === orderedArray[midpoint]) {
+    return midpoint;
+  // Recurse into the higher half
+  } else if (compare(item, orderedArray[midpoint])) {
+    return binarySearch(orderedArray, item, compare, midpoint + 1, end);
+  // Recurse into the lower half
+  } else {
+    return binarySearch(orderedArray, item, compare, start, midpoint - 1);
+  }
 }
